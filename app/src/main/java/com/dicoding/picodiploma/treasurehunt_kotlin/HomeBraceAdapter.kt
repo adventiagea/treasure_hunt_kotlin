2f60ac95971ab7427416fcdf8d2ec3a93790ee73
adventiagea <adventiagea@gmail.com>
2022-08-11 18:46:03 +0700

    home ok (minus intent from brace)

---
+++ app/src/main/java/com/dicoding/picodiploma/treasurehunt_kotlin/HomeBraceAdapter.kt
@@ -0,0 +1,34 @@
+package com.dicoding.picodiploma.treasurehunt_kotlin
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.dicoding.picodiploma.treasurehunt_kotlin.databinding.ItemBraceHomeBinding
+
+class HomeBraceAdapter(private val item : List<BraceData>) : RecyclerView.Adapter<HomeBraceAdapter.BraceViewHolder>() {
+    inner class BraceViewHolder (itemView : ItemBraceHomeBinding) : RecyclerView.ViewHolder(itemView.root) {
+        private val binding = itemView
+        fun bind(data: BraceData){
+            with(binding){
+                Glide.with(itemView)
+                    .load(data.imageBrace)
+                    .into(braceImage)
+
+                itemView.setOnClickListener {
+
+                }
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BraceViewHolder {
+        return BraceViewHolder(ItemBraceHomeBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+    }
+
+    override fun onBindViewHolder(holder: BraceViewHolder, position: Int) {
+        holder.bind(item[position])
+    }
+
+    override fun getItemCount(): Int = item.size
+}
--- app/src/main/java/com/dicoding/picodiploma/treasurehunt_kotlin/MainActivity.kt
+++ app/src/main/java/com/dicoding/picodiploma/treasurehunt_kotlin/MainActivity.kt
@@ -1,12 +1,81 @@
 package com.dicoding.picodiploma.treasurehunt_kotlin
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Html
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.viewpager2.widget.ViewPager2
+import com.dicoding.picodiploma.treasurehunt_kotlin.databinding.ActivityMainBinding
+import com.dicoding.picodiploma.treasurehunt_kotlin.databinding.ActivityOnBoardBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+    private lateinit var adapter: HomeBraceAdapter
+    private val list = ArrayList<BraceData>()
+    private lateinit var dot : ArrayList<TextView>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        list.add(
+            BraceData(
+                R.drawable.brace1
+            )
+        )
+
+        list.add(
+            BraceData(
+                R.drawable.brace2
+            )
+        )
+
+        list.add(
+            BraceData(
+                R.drawable.brace3
+            )
+        )
+
+        adapter = HomeBraceAdapter(list)
+        binding.viewPagerHome.adapter = adapter
+        dot = ArrayList()
+
+        setIndicator()
+
+        binding.viewPagerHome.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback(){
+            override fun onPageSelected(position: Int) {
+                selectedImage(position)
+                super.onPageSelected(position)
+            }
+        })
+    }
+
+    private fun selectedImage(position: Int) {
+        for (i in 0 until list.size){
+            if (i == position){
+                dot[i].setTextColor(ContextCompat.getColor(this, R.color.yellow))
+            }
+            else{
+                dot[i].setTextColor(ContextCompat.getColor(this, R.color.text))
+            }
+        }
+    }
+
+    private fun setIndicator() {
+        for(i in 0 until list.size){
+            dot.add(TextView(this))
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                dot[i].text = Html.fromHtml("&#9679", Html.FROM_HTML_MODE_LEGACY).toString()
+            }
+            else {
+                dot[i].text = Html.fromHtml("&#9679")
+            }
+
+            dot[i].textSize = 12f
+            binding.indikatorHome.addView(dot[i])
+        }
     }
 }
