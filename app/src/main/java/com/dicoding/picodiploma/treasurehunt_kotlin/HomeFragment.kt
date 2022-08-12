c8e6e609e8aff0417421b6fe7d97cdb4f9651062
adventiagea <adventiagea@gmail.com>
2022-08-12 12:15:39 +0700

    bottom navigation

---
+++ app/src/main/java/com/dicoding/picodiploma/treasurehunt_kotlin/HomeFragment.kt
@@ -0,0 +1,133 @@
+package com.dicoding.picodiploma.treasurehunt_kotlin
+
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import android.text.Editable
+import android.text.Html
+import android.text.TextWatcher
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.*
+import androidx.core.content.ContextCompat
+import androidx.viewpager2.widget.ViewPager2
+import com.dicoding.picodiploma.treasurehunt_kotlin.databinding.ActivityMainBinding
+import com.dicoding.picodiploma.treasurehunt_kotlin.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+    private lateinit var adapter: HomeBraceAdapter
+    private val list = ArrayList<BraceData>()
+    private lateinit var dot : ArrayList<TextView>
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ) : View {
+        val binding = FragmentHomeBinding.inflate(inflater, container, false) // Inflate the layout for this fragment
+
+        activity?.actionBar?.hide()
+
+        list.add(
+            BraceData(
+                R.drawable.brace1
+            )
+        )
+
+        list.add(
+            BraceData(
+                R.drawable.brace2
+            )
+        )
+
+        list.add(
+            BraceData(
+                R.drawable.brace3
+            )
+        )
+
+        adapter = HomeBraceAdapter(list)
+        binding.viewPagerHome.adapter = adapter
+        dot = ArrayList()
+
+        setIndicator()
+
+        binding.viewPagerHome.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback(){
+            override fun onPageSelected(position: Int) {
+                selectedImage(position)
+                super.onPageSelected(position)
+            }
+        })
+
+        val inputCode = binding.inputCode
+        val playButton = binding.playButton
+
+        inputCode.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                playButton.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.login_gray))
+                playButton.isClickable = false
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                playButton.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.green))
+
+                play()
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                playButton.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.green))
+
+                play()
+            }
+
+        })
+
+        return binding.root
+    }
+
+    private fun play() {
+        val inputCode = view?.findViewById<EditText>(R.id.input_code)
+        val playButton = view?.findViewById<Button>(R.id.play_button)
+
+        playButton?.setOnClickListener{
+            if (inputCode?.text.toString().isNotEmpty()){
+                val intent = Intent(requireContext(), LobbyActivity::class.java)
+
+                startActivity(intent)
+            }
+            else{
+                Toast.makeText(requireContext(), "Masukkan Kode Permainan!", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
+
+    private fun selectedImage(position: Int) {
+        for (i in 0 until list.size){
+            if (i == position){
+                dot[i].setTextColor(ContextCompat.getColor(requireContext(), R.color.yellow))
+            }
+            else{
+                dot[i].setTextColor(ContextCompat.getColor(requireContext(), R.color.text))
+            }
+        }
+    }
+
+    private fun setIndicator() {
+
+        for(i in 0 until list.size){
+            dot.add(TextView(requireContext()))
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                dot[i].text = Html.fromHtml("&#9679", Html.FROM_HTML_MODE_LEGACY).toString()
+            }
+            else {
+                dot[i].text = Html.fromHtml("&#9679")
+            }
+
+            dot[i].textSize = 12f
+            view?.findViewById<LinearLayout>(R.id.indikator_home)?.addView(dot[i])
+        }
+    }
+
+}
--- app/src/main/java/com/dicoding/picodiploma/treasurehunt_kotlin/MainActivity.kt
+++ app/src/main/java/com/dicoding/picodiploma/treasurehunt_kotlin/MainActivity.kt
@@ -10,15 +10,15 @@ import android.text.TextWatcher
 import android.widget.TextView
 import android.widget.Toast
 import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
 import androidx.viewpager2.widget.ViewPager2
 import com.dicoding.picodiploma.treasurehunt_kotlin.databinding.ActivityMainBinding
-import com.dicoding.picodiploma.treasurehunt_kotlin.databinding.ActivityOnBoardBinding
+import com.dicoding.picodiploma.treasurehunt_kotlin.databinding.FragmentHomeBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
-    private lateinit var adapter: HomeBraceAdapter
-    private val list = ArrayList<BraceData>()
-    private lateinit var dot : ArrayList<TextView>
+    private lateinit var bottomNav : BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,101 +27,34 @@ class MainActivity : AppCompatActivity() {
 
         supportActionBar?.hide()
 
-        list.add(
-            BraceData(
-                R.drawable.brace1
-            )
-        )
+        bottomNav = binding.bottomNav
+        loadFragment(HomeFragment())
 
-        list.add(
-            BraceData(
-                R.drawable.brace2
-            )
-        )
-
-        list.add(
-            BraceData(
-                R.drawable.brace3
-            )
-        )
-
-        adapter = HomeBraceAdapter(list)
-        binding.viewPagerHome.adapter = adapter
-        dot = ArrayList()
-
-        setIndicator()
-
-        binding.viewPagerHome.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback(){
-            override fun onPageSelected(position: Int) {
-                selectedImage(position)
-                super.onPageSelected(position)
-            }
-        })
-
-        val inputCode = binding.inputCode
-        val playButton = binding.playButton
-
-        inputCode.addTextChangedListener(object : TextWatcher{
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-                playButton.setBackgroundColor(ContextCompat.getColor(this@MainActivity, R.color.login_gray))
-                playButton.isClickable = false
-            }
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                playButton.setBackgroundColor(ContextCompat.getColor(this@MainActivity, R.color.green))
-
-                play()
-            }
-
-            override fun afterTextChanged(s: Editable?) {
-                playButton.setBackgroundColor(ContextCompat.getColor(this@MainActivity, R.color.green))
-
-                play()
-            }
-
-        })
-    }
-
-    private fun play() {
-        val inputCode = binding.inputCode
-        val playButton = binding.playButton
-
-        playButton.setOnClickListener{
-            if (inputCode.text.toString().isNotEmpty()){
-                val intent = Intent(this, LobbyActivity::class.java)
-
-                startActivity(intent)
-            }
-            else{
-                Toast.makeText(this, "Masukkan Kode Permainan!", Toast.LENGTH_SHORT).show()
+        bottomNav.setOnNavigationItemReselectedListener {
+            when (it.itemId){
+                R.id.home -> {
+                    loadFragment(HomeFragment())
+                    return@setOnNavigationItemReselectedListener
+                }
+                R.id.games -> {
+                    loadFragment(GamesFragment())
+                    return@setOnNavigationItemReselectedListener
+                }
+                R.id.account -> {
+                    loadFragment(AccountFragment())
+                    return@setOnNavigationItemReselectedListener
+                }
             }
         }
 
     }
 
-    private fun selectedImage(position: Int) {
-        for (i in 0 until list.size){
-            if (i == position){
-                dot[i].setTextColor(ContextCompat.getColor(this, R.color.yellow))
-            }
-            else{
-                dot[i].setTextColor(ContextCompat.getColor(this, R.color.text))
-            }
-        }
+    private fun loadFragment(fragment : Fragment){
+        val transaction = supportFragmentManager.beginTransaction()
+
+        transaction.replace(R.id.container, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
     }
 
-    private fun setIndicator() {
-        for(i in 0 until list.size){
-            dot.add(TextView(this))
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-                dot[i].text = Html.fromHtml("&#9679", Html.FROM_HTML_MODE_LEGACY).toString()
-            }
-            else {
-                dot[i].text = Html.fromHtml("&#9679")
-            }
-
-            dot[i].textSize = 12f
-            binding.indikatorHome.addView(dot[i])
-        }
-    }
 }
