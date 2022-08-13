5f8694835687f00790eeef673b24c41fc98d422d
adventiagea <adventiagea@gmail.com>
2022-08-14 02:47:59 +0700

    BRACE

---
+++ app/src/main/java/com/dicoding/picodiploma/treasurehunt_kotlin/brace/BraceCheckInActivity.kt
@@ -0,0 +1,11 @@
+package com.dicoding.picodiploma.treasurehunt_kotlin.brace
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BraceCheckInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_brace_chek_in)
+    }
+}
