2b1995b320214fe951ae3a0ca0a1fedc4c1bf01b
JunarisAlf <junaris0715@gmail.com>
2022-08-13 23:40:53 +0700

    Finish Game Post endpoint

--- app/src/main/java/com/dicoding/picodiploma/treasurehunt_kotlin/ApiTestActivity.kt
+++ app/src/main/java/com/dicoding/picodiploma/treasurehunt_kotlin/ApiTestActivity.kt
@@ -9,6 +9,7 @@ import com.dicoding.picodiploma.treasurehunt_kotlin.api.auth.login.LoginBody
 import com.dicoding.picodiploma.treasurehunt_kotlin.api.auth.registration.RegisterBody
 import com.dicoding.picodiploma.treasurehunt_kotlin.api.game_control.GameControlInterface
 import com.dicoding.picodiploma.treasurehunt_kotlin.api.game_control.join_game.JoinBody
+import com.dicoding.picodiploma.treasurehunt_kotlin.api.game_post.GamePostInterface
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import com.dicoding.picodiploma.treasurehunt_kotlin.api.games.GameInterface as GameInterface1
@@ -17,9 +18,12 @@ class ApiTestActivity : AppCompatActivity()  {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Defined Interface
         val auth = RetrofitClient.init().create(AuthInterface::class.java)
         val games = RetrofitClient.init().create(GameInterface1::class.java)
         val gameControl = RetrofitClient.init().create(GameControlInterface::class.java)
+        val gamePost = RetrofitClient.init().create(GamePostInterface::class.java)
 
         GlobalScope.launch {
             // AUTH
@@ -53,6 +57,16 @@ class ApiTestActivity : AppCompatActivity()  {
             val lobbyID = joinRes.body()?.data?.lobby_id
             val lobbyDetailRes = gameControl.getLobbyDetail(token, lobbyID.toString(), game_token.toString())
             Log.d("API-lobby detail: ", lobbyDetailRes.body().toString())
+
+            //GAME POST
+            //game id yg didapat setelah join room/lobby/game
+            val gameID2 = joinRes.body()?.data?.game_id.toString()
+            val postListRes = gamePost.getPostList(token, gameID2)
+            Log.d("API-post list: ", postListRes.body().toString())
+
+            val postID = postListRes.body()?.data!![0].id
+            val postDetailRes = gamePost.getPostDetail(token, postID)
+            Log.d("API-post detail: ", postDetailRes.body().toString())
 
         }
     }
