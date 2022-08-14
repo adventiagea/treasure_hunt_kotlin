3d0fe6ce4f46ad249e25b5eb4bfef3da04ad40ca
WnB <wahyubahri1212@gmail.com>
2022-08-15 06:48:49 +0700

    game-control hit endpoint untuk game

--- app/src/main/java/com/dicoding/picodiploma/treasurehunt_kotlin/api/game_control/GameControlInterface.kt
+++ app/src/main/java/com/dicoding/picodiploma/treasurehunt_kotlin/api/game_control/GameControlInterface.kt
@@ -1,9 +1,12 @@
 package com.dicoding.picodiploma.treasurehunt_kotlin.api.game_control
 
+import com.dicoding.picodiploma.treasurehunt_kotlin.api.game_control.check_progress.CheckProgress
 import com.dicoding.picodiploma.treasurehunt_kotlin.api.game_control.join_game.Join
 import com.dicoding.picodiploma.treasurehunt_kotlin.api.game_control.join_game.JoinBody
 import com.dicoding.picodiploma.treasurehunt_kotlin.api.game_control.lobby.Lobby
 import com.dicoding.picodiploma.treasurehunt_kotlin.api.game_control.me.Me
+import com.dicoding.picodiploma.treasurehunt_kotlin.api.game_control.next_flow.Flow
+import com.dicoding.picodiploma.treasurehunt_kotlin.api.game_control.next_flow.NextFlow
 import com.dicoding.picodiploma.treasurehunt_kotlin.api.game_control.ready_check.ReadyCheck
 import com.dicoding.picodiploma.treasurehunt_kotlin.api.game_control.start_game.StartGame
 import retrofit2.Response
@@ -32,6 +35,15 @@ interface GameControlInterface {
     @POST("/mobile/v1/game-controls/start-game")
     fun startGame(@Header("Authorization") token: String,
                    @Query("game_token") game_token: String) : Response<StartGame>
+
+    @POST("/mobile/v1/game-controls/next-flow")
+    fun nextFlow(@Header("Authorization") token: String,
+                 @Query("game_token") game_token: String,
+                 @Body flow_id : JoinBody) : Response<NextFlow>
+
+    @POST("/mobile/v1/game-controls/next-flow")
+    fun checkProgress(@Header("Authorization") token: String,
+                 @Query("game_token") game_token: String) : Response<CheckProgress>
 /*
     @POST("/mobile/v1/game-controls/start-game")
     fun startGame(@Header("Authorization") token: String,
