8c0e63118f8a429e6c84e99b3b27696c52ddfc21
adventiagea <adventiagea@gmail.com>
2022-08-14 12:05:17 +0700

    manohara

---
+++ app/src/main/java/com/dicoding/picodiploma/treasurehunt_kotlin/manohara/ManoharaMainActivity.kt
@@ -0,0 +1,12 @@
+package com.dicoding.picodiploma.treasurehunt_kotlin.manohara
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dicoding.picodiploma.treasurehunt_kotlin.R
+
+class ManoharaMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_manohara_main)
+    }
+}
