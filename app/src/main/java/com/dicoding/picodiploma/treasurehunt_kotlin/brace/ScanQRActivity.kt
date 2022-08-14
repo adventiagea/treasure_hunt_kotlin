ab5ed2d592209902609bda5db855f0ce7a0b3b31
adventiagea <adventiagea@gmail.com>
2022-08-14 12:39:37 +0700

    qr

--- app/src/main/java/com/dicoding/picodiploma/treasurehunt_kotlin/brace/ScanQRActivity.kt
+++ app/src/main/java/com/dicoding/picodiploma/treasurehunt_kotlin/brace/ScanQRActivity.kt
@@ -1,8 +1,12 @@
 package com.dicoding.picodiploma.treasurehunt_kotlin.brace
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.budiyev.android.codescanner.AutoFocusMode
 import com.budiyev.android.codescanner.CodeScanner
 import com.budiyev.android.codescanner.CodeScannerView
@@ -17,6 +21,11 @@ class ScanQRActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_scan_qr_activity)
+
+        setupPermissions()
+
+        codeScanner()
+        /*
 
         val scannerView = findViewById<CodeScannerView>(R.id.scanner_view)
 
@@ -47,6 +56,10 @@ class ScanQRActivity : AppCompatActivity() {
         scannerView.setOnClickListener {
             codeScanner.startPreview()
         }
+
+         */
+
+
     }
     override fun onResume() {
         super.onResume()
@@ -56,5 +69,69 @@ class ScanQRActivity : AppCompatActivity() {
     override fun onPause() {
         codeScanner.releaseResources()
         super.onPause()
+    }
+
+    private fun codeScanner() {
+        val scannerView = findViewById<CodeScannerView>(R.id.scanner_view)
+        codeScanner = CodeScanner(this, scannerView)
+
+        codeScanner.apply {
+            camera = CodeScanner.CAMERA_BACK
+            formats = CodeScanner.ALL_FORMATS
+
+            autoFocusMode = AutoFocusMode.SAFE
+            scanMode = ScanMode.CONTINUOUS
+            isAutoFocusEnabled = true
+            isFlashEnabled = false
+
+            errorCallback = ErrorCallback {
+                runOnUiThread {
+                    Log.e("Main", "codeScanner: ${it.message}")
+                }
+            }
+
+            scannerView.setOnClickListener {
+                codeScanner.startPreview()
+            }
+
+        }
+    }
+
+    private fun setupPermissions() {
+        val permission = ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA)
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            makeRequest()
+        }
+    }
+
+    private fun makeRequest() {
+        ActivityCompat.requestPermissions(
+            this, arrayOf(android.Manifest.permission.CAMERA),
+            CAMERA_REQ
+        )
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            CAMERA_REQ -> {
+                if (grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(
+                        this,
+                        "You need the camera permission to use this app",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+        }
+    }
+
+    companion object {
+        private const val CAMERA_REQ = 101
     }
 }
